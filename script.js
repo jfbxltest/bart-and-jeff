7d90999ea9abf5ff53a2540c60d59eb981e02c7e
jfl <jeanfebxl@hotmail.fr>
2024-04-21 17:42:58 +0200

    first

---
+++ script.js
@@ -0,0 +1,42 @@
+function calculPoints(formula, min = -100, max = 100, step = 2) {
+  if (typeof formula !== "function") return []
+  const pts = []
+  for (let i = min; i <= max; i += step) {
+    const result = formula(i / 100)
+    if (Number.isFinite(result)) {
+      pts.push([i, result])
+    }
+  }
+  return pts
+}
+
+// template
+const getFunctionGraph =
+  (formula) =>
+    (min, max) =>
+      (args, step) => calculPoints(formula(args), min, max, step)
+
+
+// definitions ("Variation")
+const formulaVariation = (d = 1) => (x) => 2 * d / (1 - x * x)
+
+// instanciation
+const graphVariation = (min, max) => getFunctionGraph(formulaVariation)(min, max)
+
+
+
+
+// const svgPolylineElem = document.querySelector('svg>polyline')
+// svgPolylineElem.setAttribute("points", getPointsPolyline(graphVariation()()))
+
+function getPointsPolyline(pts) {
+  const mapped = pts.map(pt => `${pt[0]},${pt[1]}`)
+  return mapped.join(' ')
+}
+
+
+function drawVariationIntoPolyline(polyline, v, d, p) {
+  console.log('here************', v, d, p)
+  polyline.setAttribute("points", getPointsPolyline(graphVariation()(d / v, p)))
+}
+
