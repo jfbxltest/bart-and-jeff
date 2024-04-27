c789443dd8bc562689444bfd03430af9df574074
1402jelorand <jeanfebxl@hotmail.fr>
2024-04-27 10:44:35 +0200

    universel unary minus operator

--- scripts/calculatrice.js
+++ scripts/calculatrice.js
@@ -14,18 +14,18 @@ const op = [
 
 function getValue(p) {
   let value = 0;
-  let sign = 1;
-  console.log("get value ******");
+
   if (p[0] == "-") {
     p.shift();
-    sign = -1;
+    //universel unary minus operator
+    return [op["*"], -1, getValue];
   }
 
   if (p[0] == "(") {
     p.shift();
     value = getExpr(p);
     p.shift();
-    return sign * value;
+    return value;
   }
 
   while (!isNaN(p[0])) {
@@ -52,7 +52,6 @@ function getValue(p) {
 function factorize(p, n) {
   let expr = [null, null, null];
   expr[1] = n > 0 ? factorize(p, n - 1) : getValue(p);
-  // console.log("oprateur ", p, n);
   if (op[n][p[0]]) {
     expr[0] = op[n][p.shift()];
     expr[2] = factorize(p, n);
@@ -61,6 +60,9 @@ function factorize(p, n) {
   return expr[1];
 }
 
+/**
+ *  USAGE
+ */
 function calcuExpression(phrase) {
   solve = (exp) => {
     if (exp.length) {
@@ -73,7 +75,7 @@ function calcuExpression(phrase) {
   return solve(factorize(phrase.split(""), 2));
 }
 
-/*
+/* OLD - FIRST
 const getFact = (p) => {
   let fact = [null, null, null];
   fact[1] = getValue(p);
